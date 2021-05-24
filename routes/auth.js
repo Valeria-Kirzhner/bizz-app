43c454854f777e0fbfa78016c25f11f2d16fad5c
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-24 22:26:35 +0300

    in case of user succeed to connect - the responce changed to be a token

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
    
-    res.send('ok!');
+      res.json({ token: user.generateAuthToken() });
    
   });
    
