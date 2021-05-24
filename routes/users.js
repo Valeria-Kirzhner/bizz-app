db324de8b132ab5cb3b2f9b8fb72ac27d9bb92e6
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-24 21:16:32 +0300

    added to the anpoint of saving user (POST)  if condition for chacking for if email exist?

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,10 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered.');
+  
 });
 
 module.exports = router;
