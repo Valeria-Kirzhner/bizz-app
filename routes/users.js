d09d603fd2cb1c27046f656bfbaea0acad263374
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-24 23:16:46 +0300

    added user route (GET) /me to recieve all user info search by user's id and only after auth middleware token

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const router = express.Router();
 
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
