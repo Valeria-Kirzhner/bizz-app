5a0ba04682648b9ad49e3dbd6e068f0e35ab3cf0
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-24 21:22:48 +0300

    added to the anpoint of saving user (POST) using req.body + bycrypt + sending user paiload

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,12 @@ router.post('/', async (req, res) => {
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
+
+  user = new User(req.body);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+  res.send(user);
   
 });
 
