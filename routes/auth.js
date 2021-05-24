b06a017a771134a9b929a3d87bb75564be400162
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-24 22:12:57 +0300

    added to auth.js new andpoint (POST) for connecting user after validation and comparing password with bcrypt.compare()

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,29 @@ const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+router.post('/', async (req, res) => {
+ 
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+   
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+   
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+   
+    res.send('ok!');
+   
+  });
+   
+  function validate(req) {
+   
+    const schema = Joi.object({
+      email: Joi.string().min(6).max(255).required().email(),
+      password: Joi.string().min(6).max(1024).required()
+    });
+   
+    return schema.validate(req);
+  }
+   
+  module.exports = router;
