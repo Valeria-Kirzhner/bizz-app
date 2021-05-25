94307e8413acecf94ee76f1878c0988778039d44
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-25 08:47:20 +0300

    added a new model card to new file card.js, include cardSchema from mongoose and joi validation & reqex

---
+++ models/card.js
@@ -0,0 +1,60 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  bizName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 255
+  },
+  bizDescription: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 1024
+  },
+  bizAddress: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 400
+  },
+  bizPhone: {
+    type: String,
+    required: true,
+    minlength: 9,
+    maxlength: 10
+  },
+  bizImage: {
+    type: String,
+    required: true,
+    minlength: 11,
+    maxlength: 1024
+  },
+  bizNumber: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 99999999999,
+    unique: true
+  }
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+function validateCard(card) {
+
+  const schema = Joi.object({
+    bizName: Joi.string().min(2).max(255).required(),
+    bizDescription: Joi.string().min(2).max(1024).required(),
+    bizAddress: Joi.string().min(2).max(400).required(),
+    bizPhone: Joi.string().min(9).max(10).required().regex(/^0[2-9]\d{7,8}$/),
+    bizImage: Joi.string().min(11).max(1024)
+  });
+
+  return schema.validate(card);
+}
+
+exports.Card = Card;
+exports.validateCard = validateCard;
