55855d3c7252fd1e22152e9222502cef29593c38
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-25 15:28:06 +0300

    a new andpoint was added (PUT) to cards route to update card in db. Only card that matches the card.id+user.id and have token and past validation

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,21 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 
+router.put('/:id', auth, async (req, res) => {
+ 
+    const { error } = validateCard(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+   
+    let card = await Card.findOneAndUpdate({ _id: req.params.id, user_id: req.user._id }, req.body);
+    if (!card) return res.status(404).send('The card with the given ID was not found.');
+   
+    card = await Card.findOne({ _id: req.params.id, user_id: req.user._id });
+    res.send(card);
+   
+  });
+
+  
+
 router.get('/:id', auth, async (req, res) => {
  
     const card = await Card.findOne({ _id: req.params.id, user_id: req.user._id });
@@ -13,7 +28,7 @@ router.get('/:id', auth, async (req, res) => {
    
   });
 
-  
+
 
 router.post('/', auth, async (req, res) => {
 
