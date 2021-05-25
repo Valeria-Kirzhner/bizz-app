1bda180463f21a1e4ffb46a82a5069525e8e4772
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-25 09:05:42 +0300

    added to route cards a first andpoint (POST) to create new card, and run generateBizNumber function

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const _ = require('lodash');
-const { Card, validateCard } = require('../models/card');
+const { Card, validateCard, generateBizNumber } = require('../models/card');
 const auth = require('../middleware/auth');
 const router = express.Router();
+
+router.post('/', auth, async (req, res) => {
+ 
+    const randomNumber = await generateBizNumber(Card);
+   
+    res.send(randomNumber);
+   
+  });
 
 module.exports = router;
