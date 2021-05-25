51146091ce1131fdc8d66b2bd563f08d57ab155a
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-25 11:42:00 +0300

    new endpoint was added to card route (POST) after validation and generate by user schema. The paiload will include card details+bizNumber+ref.user._id

--- routes/cards.js
+++ routes/cards.js
@@ -5,11 +5,25 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/', auth, async (req, res) => {
- 
-    const randomNumber = await generateBizNumber(Card);
-   
-    res.send(randomNumber);
-   
-  });
+
+  const { error } = validateCard(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let card = new Card(
+    {
+      bizName: req.body.bizName,
+      bizDescription: req.body.bizDescription,
+      bizAddress: req.body.bizAddress,
+      bizPhone: req.body.bizPhone,
+      bizImage: req.body.bizImage ? req.body.bizImage : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+      bizNumber: await generateBizNumber(Card),
+      user_id: req.user._id
+    }
+  );
+
+  post = await card.save();
+  res.send(post);
+
+});
 
 module.exports = router;
