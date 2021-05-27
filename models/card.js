ade5706d99bb99d0aa8ae87bae0a5355e2a89537
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-27 18:48:01 +0300

    correction

--- models/card.js
+++ models/card.js
@@ -1,71 +1,72 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const _ = require('lodash');
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const cardSchema = new mongoose.Schema({
   bizName: {
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 255
+    maxlength: 255,
   },
   bizDescription: {
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 1024
+    maxlength: 1024,
   },
   bizAddress: {
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 400
+    maxlength: 400,
   },
   bizPhone: {
     type: String,
     required: true,
     minlength: 9,
-    maxlength: 10
+    maxlength: 10,
   },
   bizImage: {
     type: String,
     required: true,
     minlength: 11,
-    maxlength: 1024
+    maxlength: 1024,
   },
   bizNumber: {
     type: String,
     required: true,
     minlength: 3,
     maxlength: 99999999999,
-    unique: true
+    unique: true,
   },
-   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
-const Card = mongoose.model('Card', cardSchema);
+const Card = mongoose.model("Card", cardSchema);
 
 function validateCard(card) {
-
   const schema = Joi.object({
     bizName: Joi.string().min(2).max(255).required(),
     bizDescription: Joi.string().min(2).max(1024).required(),
     bizAddress: Joi.string().min(2).max(400).required(),
-    bizPhone: Joi.string().min(9).max(10).required().regex(/^0[2-9]\d{7,8}$/),
-    bizImage: Joi.string().min(11).max(1024)
+    bizPhone: Joi.string()
+      .min(9)
+      .max(10)
+      .required()
+      .regex(/^0[2-9]\d{7,8}$/),
+    bizImage: Joi.string().min(11).max(1024),
   });
 
   return schema.validate(card);
 }
 
 async function generateBizNumber(Card) {
- 
   while (true) {
     let randomNumber = _.random(1000, 999999);
     let card = await Card.findOne({ bizNumber: randomNumber });
     if (!card) return String(randomNumber);
   }
- 
 }
 
 exports.Card = Card;
