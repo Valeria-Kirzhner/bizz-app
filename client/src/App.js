0cc7366ab3eec49155b073c76af922f0bea7879f
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-28 21:54:35 +0300

    3 layouts was added to app.js - header , main and footer.

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-primary">my app</p>
-      <i class="fas fa-user"></i>
-    </div>
+    <React.Fragment>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </React.Fragment>
   );
 }
 
