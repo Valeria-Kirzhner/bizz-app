15e153c2cf93fec96267bcf04945c1246d2a3c1c
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-29 15:04:15 +0300

    a content was added to About component

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -7,12 +7,30 @@ class About extends Component {
       <div className="container">
         <div className="row">
           <div className="col-12 mt-4">
-            <h1>About Real App</h1>
+            <h1>About Bizz App</h1>
           </div>
         </div>
         <div className="row">
           <div className="col-12">
-            <p>Content example text for about page here.</p>
+            <p>
+              We all know that creating fisical bussiness cards it's cost some
+              money, sometimes they are gone and we need to remember to order
+              new quality. And sometimes we just forgot the costume in the
+              office and we extrimely need to give someone our card right now.
+            </p>
+            <p>
+              Bizz app was created to give you comfortible platform to your
+              bussiness card in your phone.{" "}
+            </p>
+            <p>
+              You can create one or more cards and share theme with everyone who
+              have registred in Bizz app.
+            </p>
+            <p>
+              Your partner can save your card in his account either you can save
+              some other user's card in your account and use see theme when ever
+              you want. No more drugging a pursle with hundred of cards.
+            </p>
           </div>
         </div>
       </div>
