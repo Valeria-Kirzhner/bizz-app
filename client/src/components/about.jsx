cf1b0553dbfaae32fa06a45ddba726909812d0e1
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-29 14:24:02 +0300

    About component was created

---
+++ client/src/components/about.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mt-4">
+            <h1>About Real App</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <p>Content example text for about page here.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
