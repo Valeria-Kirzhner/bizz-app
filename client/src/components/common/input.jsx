0444120e74a7a877aff0bf2317d6f72a074e841a
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-29 17:13:51 +0300

    Input component created in common folder to render generic html input to avoid duplacion of the code.

---
+++ client/src/components/common/input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = ({ name, label, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input {...rest} name={name} id={name} className="form-control" />
+      {error && <span className="text-danger">{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
