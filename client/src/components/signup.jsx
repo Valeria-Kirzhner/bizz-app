61b7ed7037d18e20789bb667ebfc6b3e3995b0b7
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-29 17:33:37 +0300

    Signup component extends changed from React to Form generic component. Property data & errors was added to the state. Schema for the data was created and html form with method POST with few inputs added to the render.

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,8 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PageHeader from "./common/pageHeader";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class Signup extends Component {
-  state = {};
+class Signup extends Form {
+  state = {
+    data: { email: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    email: Joi.string().required().email().label("Email"),
+    password: Joi.string().required().min(6).label("Password"),
+    name: Joi.string().required().min(2).label("Name"),
+  };
+
+  doSubmit = () => {
+    // Call the server
+    console.log("Submitted");
+  };
   render() {
     return (
       <div className="container">
@@ -15,6 +31,14 @@ class Signup extends Component {
         <div className="row">
           <div className="col-12">
             <p>You can open new account for free!</p>
+          </div>
+          <div className="col-lg-6">
+            <form onSubmit={this.handleSubmit} autoComplete="off" method="POST">
+              {this.renderInput("email", "Email", "email")}
+              {this.renderInput("password", "Password", "password")}
+              {this.renderInput("name", "Name")}
+              {this.renderButton("Signup")}
+            </form>
           </div>
         </div>
       </div>
