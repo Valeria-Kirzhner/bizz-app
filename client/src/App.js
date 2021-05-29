b3718db61a59357c98ab0c3bb42ca3b8cf1f6d2d
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-29 14:11:08 +0300

    Home component was added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import Home from "./components/home";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <header>
         <Navbar />
       </header>
-      <main style={{ minHeight: 900 }}></main>
+      <main style={{ minHeight: 900 }}>
+        <Home />
+      </main>
       <footer>
         <Footer />
       </footer>
---
+++ client/src/components/home.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mt-4">
+            <h1>Biz App Home Page</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h3>It's all about bussiness</h3>
+            <h4>pDiscover new possibilities </h4>
+            <h4>Create your own card </h4>
+            <h4>Share </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
