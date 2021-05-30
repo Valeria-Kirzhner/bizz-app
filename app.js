48b0f82d723a86ca45e06a185f2bacf10d518d44
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-30 20:47:44 +0300

    cors was required to app.js and used as a middlesere.

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const cards = require('./routes/cards');
-const express = require('express');
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const cards = require("./routes/cards");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const http = require('http').Server(app);
-const mongoose = require('mongoose');
+const http = require("http").Server(app);
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/biz_app_api', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+mongoose
+  .connect("mongodb://localhost/biz_app_api", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
 
-app.use(express.json());// midelware that make all req & res in the app be JSON type only.
+app.use(cors());
+app.use(express.json()); // midelware that make all req & res in the app be JSON type only.
 
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/api/cards', cards);
-
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/cards", cards);
 
 const port = 3900;
 http.listen(port, () => console.log(`Listening on port ${port}...`));
