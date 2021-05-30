3b5a65dbc0fe188f6e80d1820f8b9749f2c05993
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-30 21:28:54 +0300

    axios.interseptore was used in httpService.js to take care of responses that are equal or  more then status 403 and take care in case response doesn't exist.It will throw toast message and promise reject.

---
+++ client/src/services/httpServices.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError = error.response && error.response.status >= 403;
+  if (expectedError) toast.error("An unexpected error occurrred.");
+  return Promise.reject(error);
+});
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  patch: axios.patch,
+  delete: axios.delete,
+};
+export default http;
