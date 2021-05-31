a2eb449d44807c256070528abee3e4a420a61f89
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-05-31 09:13:45 +0300

    girCurrentUser function was added to try to extract the tokeyKey if case that it exist.Then it return decode of the token.

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,17 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+import jwtDecode from "jwt-decode";
 
 const tokenKey = "token";
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
 
 export async function login(email, password) {
   const { data } = await http.post(`${apiUrl}/auth`, { email, password });
@@ -10,5 +20,6 @@ export async function login(email, password) {
 
 const userService = {
   login,
+  getCurrentUser,
 };
 export default userService;
