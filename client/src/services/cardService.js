6287c3e49e474c4fbd3bc87757eb70439f3816c2
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-02 19:08:15 +0300

    created getMyCards function in cardServise. It's return get request to apiUrl/cards/my-cards.

--- client/src/services/cardService.js
+++ client/src/services/cardService.js
@@ -1,12 +1,17 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+export function getMyCards() {
+  return http.get(`${apiUrl}/cards/my-cards`);
+}
+
 export function createCard(card) {
   return http.post(`${apiUrl}/cards`, card);
 }
 
 const cardService = {
   createCard,
+  getMyCards,
 };
 
 export default cardService;
