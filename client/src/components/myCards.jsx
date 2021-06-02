03d4db9d0824d2dc6f9a551dcf278e90fa29d344
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-02 19:16:18 +0300

    added a function on did mount to MyCards it will use getMyCards function to sent get request and only if there more then 0 it will setState, and render the card/s with Card component using the map method.

--- client/src/components/myCards.jsx
+++ client/src/components/myCards.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import PageHeader from "./common/pageHeader";
+import cardService from "../services/cardService";
+import Card from "./card";
 
 class MyCards extends Component {
-  state = {};
+  state = {
+    cards: [],
+  };
+
+  async componentDidMount() {
+    const { data } = await cardService.getMyCards();
+    if (data.length > 0) this.setState({ cards: data });
+  }
+
   render() {
+    const { cards } = this.state;
     return (
       <div className="container">
         <PageHeader titleText="My Cards Page" />
@@ -11,6 +22,10 @@ class MyCards extends Component {
           <div className="col-12">
             <p>Your cards in the list below...</p>
           </div>
+        </div>
+        <div className="row">
+          {cards.length > 0 &&
+            cards.map((card) => <Card key={card._id} card={card} />)}
         </div>
       </div>
     );
