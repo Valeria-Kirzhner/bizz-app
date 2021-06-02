d4afba9053fec412c7b9737998bdcabbe0a17e1a
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-02 11:23:47 +0300

    a field x-auth-token was added to the headers by default and it will recieve the token from the localStorage and woulb be sent in every request to the server side.

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import userService from "./userService";
+
+axios.defaults.headers.common["x-auth-token"] = userService.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError = error.response && error.response.status >= 403;
