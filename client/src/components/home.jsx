86b67ca4fd55677966a100b278aad1085f503625
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-06 19:23:54 +0300

    search input was moved to indibidual component.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,29 +1,13 @@
 import React, { Component } from "react";
 import PageHeader from "./common/pageHeader";
+import Search from "./search";
 
 class Home extends Component {
   state = {};
   render() {
     return (
       <div className="container ">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-4 col-sm-offset-4 center-block">
-              <div class="input-group rounded mt-3">
-                <input
-                  type="search"
-                  class="form-control rounded "
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="search-addon"
-                />
-                <span class="input-group-text border-0" id="search-addon">
-                  <i class="fas fa-search"></i>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Search />
         <PageHeader titleText="Bizz App Home Page" />
         <div className="row">
           <div className="col-12 mt-4">
