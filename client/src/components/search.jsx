7f21c69832accf5e7f85462f48f758913ca0f689
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-06 19:27:32 +0300

    Search component changed to be class component.

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Search = () => {
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4 col-sm-offset-4 center-block">
-          <div class="input-group rounded mt-3">
-            <input
-              type="search"
-              class="form-control rounded "
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="search-addon"
-            />
-            <span class="input-group-text border-0" id="search-addon">
-              <i class="fas fa-search"></i>
-            </span>
+class Search extends Component {
+  state = {};
+  render() {
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-sm-offset-4 center-block">
+            <div class="input-group rounded mt-3">
+              <input
+                type="search"
+                class="form-control rounded "
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
+              <span class="input-group-text border-0" id="search-addon">
+                <i class="fas fa-search"></i>
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Search;
