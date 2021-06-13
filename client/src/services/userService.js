f74756a9fa81aee0ae2e3d420232fd5926209ffa
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-13 23:30:53 +0300

    added to login function to save the wishlist from the response in the localStorage too.

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -33,6 +33,7 @@ export function getCurrentUser() {
 export async function login(email, password) {
   const { data } = await http.post(`${apiUrl}/auth`, { email, password });
   localStorage.setItem(tokenKey, data.token);
+  localStorage.setItem("wishlist", data.wishlist);
   localStorage.setItem("userInfo", JSON.stringify(getCurrentUser()));
 }
 
