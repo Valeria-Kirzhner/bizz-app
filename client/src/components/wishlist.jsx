87dbcfdd5c3910fa0930afae3e8873c9a3fd07e5
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-13 23:59:07 +0300

    in case that there is some data in the state - it would be mapped and separately rendered by the FoundCard component.

--- client/src/components/wishlist.jsx
+++ client/src/components/wishlist.jsx
@@ -6,7 +6,6 @@ import FoundCard from "./foundCard";
 class wishlist extends Component {
   state = {
     cards: [],
-    found: [],
   };
 
   async componentDidMount() {
@@ -16,6 +15,7 @@ class wishlist extends Component {
   }
 
   render() {
+    let { cards } = this.state;
     return (
       <div className="container">
         <PageHeader titleText="My wishlist cards" />
@@ -23,7 +23,9 @@ class wishlist extends Component {
           <div className="col-12 mt-4"></div>
         </div>
         <div className="row">
-          <div className="col-12"></div>
+          <div className="col-12">
+            {cards.length > 0 && cards.map((card) => <FoundCard card={card} />)}
+          </div>
         </div>
       </div>
     );
