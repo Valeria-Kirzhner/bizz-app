7f2c51721562c3d2e1a53d337e3f44e40efd2a76
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-19 16:48:04 +0300

    added to server side to auth.js to send another property in response json - users name.

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,11 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  res.json({ token: user.generateAuthToken(), wishlist: user.cards });
+  res.json({
+    token: user.generateAuthToken(),
+    wishlist: user.cards,
+    userName: user.name,
+  });
 });
 
 function validate(req) {
