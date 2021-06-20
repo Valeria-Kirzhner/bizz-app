303c0cdd8daa86d72a289a55b15bdfce952c304a
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-20 21:23:27 +0300

    code correction.

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
   componentDidMount() {
     const user = userService.getCurrentUser();
     this.setState({ user });
-    console.log(user);
   }
 
   render() {
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -22,7 +22,7 @@ class Navbar extends Component {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
