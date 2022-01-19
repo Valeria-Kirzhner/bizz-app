15f8789cfdc1e2f77af4d9a387e7e6b066b20147
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-01-19 08:14:25 +0200

    added functionality to allow adding cards to wishlist from home page.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -3,6 +3,7 @@ import PageHeader from "./common/pageHeader";
 import cardService from "../services/cardService";
 import Search from "./search";
 import FoundCard from "./foundCard";
+import { toast } from "react-toastify";
 
 class Home extends Component {
   state = {
@@ -12,6 +13,39 @@ class Home extends Component {
     const { data } = await cardService.get();
     if (data.length > 0) this.setState({ cards: data });
   }
+  check = (cardId) => {
+    let list = localStorage.getItem("wishlist");
+
+    const res = list.includes(cardId);
+
+    if (res === false) {
+      // if the wishlist array not includes the chousen card.
+
+      this.addToWishlist(cardId, list);
+    } else {
+      // if the wishlist array includes the chousen card.
+
+      this.removeFromWishlist(cardId, list);
+    }
+  };
+  addToWishlist = (thecardId, list) => {
+    list = list + `,${thecardId}`;
+    localStorage.setItem("wishlist", list);
+    this.updateDB();
+    toast("Card is added to your wishlist.");
+  };
+
+  removeFromWishlist = (thecardId, list) => {
+    list = list.replace(`,${thecardId}`, "");
+    localStorage.setItem("wishlist", list);
+    this.updateDB();
+    toast("Card is remover from your wishlist.");
+  };
+
+  updateDB = async () => {
+    const { data } = await cardService.getWishlist();
+    if (data.length > 0) this.setState({ cards: data });
+  };
 
   render() {
     let { cards } = this.state;
